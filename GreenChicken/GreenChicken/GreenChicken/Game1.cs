05766e09f1343cc48d02d8910058edbb064fafd1
ZAD-Man <The.ZAD.One@GMail.com>
2012-06-07 17:20:48 -0600

    Player starts from this morning, using inputManager component. Need Camera to continue decently.

--- GreenChicken/GreenChicken/GreenChicken/Game1.cs
+++ GreenChicken/GreenChicken/GreenChicken/Game1.cs
@@ -11,13 +11,12 @@ using Microsoft.Xna.Framework.Media;
 
 namespace GreenChicken
 {
-    /// <summary>
-    /// This is the main type for your game
-    /// </summary>
     public class Game1 : Microsoft.Xna.Framework.Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+
+        public InputManager InputManager;
 
         public Game1()
         {
@@ -25,23 +24,14 @@ namespace GreenChicken
             Content.RootDirectory = "Content";
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            InputManager.GetInstance(this);
+            Components.Add(InputManager);
 
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
         protected override void LoadContent()
         {
             // Create a new SpriteBatch, which can be used to draw textures.
@@ -49,21 +39,11 @@ namespace GreenChicken
 
             // TODO: use this.Content to load your game content here
         }
-
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// all content.
-        /// </summary>
-        protected override void UnloadContent()
+protected override void UnloadContent()
         {
             // TODO: Unload any non ContentManager content here
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
             // Allows the game to exit
