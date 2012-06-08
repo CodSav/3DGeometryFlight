c5f4fab61ada91b9e742c908d66f74f17bad1180
Meowfazar <bwilson@student.neumont.edu>
2012-06-08 12:29:06 -0700

    Trying to commit version 2.0 not committing debug folders now, or the .suo file due to conflicts every time. let me know if they're needed

--- GreenChicken/GreenChicken/GreenChicken/Basic.cs
+++ GreenChicken/GreenChicken/GreenChicken/Basic.cs
@@ -1,44 +1,108 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections;
 using Microsoft.Xna.Framework;
 
 namespace GreenChicken
 {
     public abstract class Basic
     {
-        public Matrix World { get { return GetWorld(); } protected set { _world = value; } }
-        protected abstract Matrix GetWorld();
+        #region World
+
         protected Matrix _world = Matrix.Identity;
 
-        public BoundingSphere BoundingSphere { get { return GetBoundingSphere(); } protected set { _boundingSphere = value; } }
-        protected abstract BoundingSphere GetBoundingSphere();
+        public Matrix World
+        {
+            get { return GetWorld(); }
+            protected set { _world = value; }
+        }
+
+        protected virtual Matrix GetWorld()
+        {
+            return World*Matrix.CreateTranslation(Position);
+        }
+
+        #endregion
+
+        #region BoundingSphere
+
         protected BoundingSphere _boundingSphere;
+
+        public BoundingSphere BoundingSphere
+        {
+            get { return GetBoundingSphere(); }
+            protected set { _boundingSphere = value; }
+        }
+
+        protected abstract BoundingSphere GetBoundingSphere();
+
+        #endregion
+
+        #region Collision
 
         //Properties for describing if the object can collide with other objects
         //and a list of objects it has collided with
-        public bool IsCollidable { get; set; }
         public ArrayList CollidesWith = new ArrayList();
+        public bool IsCollidable { get; set; }
 
-        public Vector3 Position { get { return GetPosition(); } set { SetPosition(value); } }
-        protected abstract Vector3 GetPosition();
+        #endregion
+
+        #region Position
+
+        protected Vector3 _position = new Vector3(0, 0, 0);
+
+        public Vector3 Position
+        {
+            get { return GetPosition(); }
+            set { SetPosition(value); }
+        }
+
+        protected virtual Vector3 GetPosition()
+        {
+            return _position;
+        }
+
         protected virtual void SetPosition(Vector3 pos)
         {
             _position = pos;
             UpdateZone();
         }
-        protected Vector3 _position = new Vector3(0,0,0);
+
+        #endregion
+
+        #region Zone
 
         //Zone is a specific area of space in which to detect collisions. 
         //Each time movement is updated, the zone should be updated.
-        public BoundingBox Zone { get { return GetZone(); } protected set { SetZone(value); }}
-        protected abstract BoundingBox GetZone();
-        protected abstract BoundingBox SetZone(BoundingBox box);
-        protected abstract void UpdateZone();
+        protected BoundingBox _zone;
 
+        public BoundingBox Zone
+        {
+            get { return GetZone(); }
+            protected set { SetZone(value); }
+        }
+
+        protected virtual BoundingBox GetZone()
+        {
+            return _zone;
+        }
+
+        protected virtual void SetZone(BoundingBox box)
+        {
+            _zone = box;
+        }
+
+        protected virtual void UpdateZone()
+        {
+            Game1.GameInstance.GetZoneOfPosition(Position);
+        }
+
+        #endregion
+
+        #region GameComponent Helpers
+
+        public abstract void Initialize();
+        protected abstract void LoadContent();
         public abstract void Update();
         public abstract void Draw(Camera camera);
+        #endregion
     }
 }
--- GreenChicken/GreenChicken/GreenChicken/BasicPrimitive.cs
+++ GreenChicken/GreenChicken/GreenChicken/BasicPrimitive.cs
@@ -1,18 +1,194 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Linq;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace GreenChicken
 {
+    public enum PrimitiveFormat
+    {
+        Color,
+        ColorTexture,
+        NormalTexture,
+        Texture
+    }
+
     public abstract class BasicPrimitive : Basic
     {
+        #region Vertex Data
+
+        protected VertexPositionColorTexture[] ColorTextureVerts;
+        protected VertexPositionColor[] ColorVerts;
+        protected PrimitiveFormat Format = PrimitiveFormat.Color;
+        protected VertexPositionNormalTexture[] NormalTextureVerts;
+        protected VertexPositionTexture[] TextureVerts;
+
+        protected PrimitiveType Type = PrimitiveType.TriangleStrip;
+        protected VertexBuffer VertexBuffer;
+
+        protected int VertexOffset;
+        protected float _boundingSphereSize;
+        protected int _primitiveCount;
+        private BasicEffect effect;
+
+        protected int PrimitiveCount
+        {
+            get { return GetPrimitiveCount(); }
+            set { _primitiveCount = value; }
+        }
+
+        protected virtual int GetPrimitiveCount()
+        {
+            int vertCount = 0;
+
+            switch (Format)
+            {
+                case PrimitiveFormat.Color:
+                    vertCount = ColorVerts.Length;
+                    break;
+                case PrimitiveFormat.ColorTexture:
+                    vertCount = ColorTextureVerts.Length;
+                    break;
+                case PrimitiveFormat.NormalTexture:
+                    vertCount = NormalTextureVerts.Length;
+                    break;
+                case PrimitiveFormat.Texture:
+                    vertCount = TextureVerts.Length;
+                    break;
+            }
+
+            switch (Type)
+            {
+                case PrimitiveType.TriangleList:
+                    vertCount /= 3;
+                    break;
+                case PrimitiveType.TriangleStrip:
+                    vertCount = (vertCount - 1)/2;
+                    break;
+                case PrimitiveType.LineList:
+                    vertCount /= 2;
+                    break;
+                case PrimitiveType.LineStrip:
+                    vertCount --;
+                    break;
+            }
+
+            return vertCount;
+        }
+
+        #endregion
+
+        #region Create Primitive Data
+
+        protected void LoadPrimitive()
+        {
+            CreateVertexArray();
+            _boundingSphereSize = GetLargestDistance();
+            switch (Format)
+            {
+                case PrimitiveFormat.Color:
+                    VertexBuffer = new VertexBuffer(Game1.GameInstance.GraphicsDevice, typeof (VertexPositionColor),
+                                                    ColorVerts.Length, BufferUsage.None);
+                    VertexBuffer.SetData(ColorVerts);
+                    break;
+                case PrimitiveFormat.ColorTexture:
+                    VertexBuffer = new VertexBuffer(Game1.GameInstance.GraphicsDevice,
+                                                    typeof (VertexPositionColorTexture),
+                                                    ColorTextureVerts.Length, BufferUsage.None);
+                    VertexBuffer.SetData(ColorTextureVerts);
+                    break;
+                case PrimitiveFormat.NormalTexture:
+                    VertexBuffer = new VertexBuffer(Game1.GameInstance.GraphicsDevice,
+                                                    typeof (VertexPositionNormalTexture),
+                                                    NormalTextureVerts.Length, BufferUsage.None);
+                    VertexBuffer.SetData(NormalTextureVerts);
+                    break;
+                case PrimitiveFormat.Texture:
+                    VertexBuffer = new VertexBuffer(Game1.GameInstance.GraphicsDevice, typeof (VertexPositionTexture),
+                                                    TextureVerts.Length, BufferUsage.None);
+                    VertexBuffer.SetData(TextureVerts);
+                    break;
+            }
+        }
+
+        protected abstract void CreateVertexArray();
+
+        protected float GetLargestDistance()
+        {
+            float longest = 0.0f;
+
+            switch (Format)
+            {
+                case PrimitiveFormat.Color:
+                    longest =
+                        ColorVerts.Select(v => Vector3.Distance(Vector3.Zero, v.Position)).Concat(new[] {longest}).Max();
+                    break;
+                case PrimitiveFormat.ColorTexture:
+                    longest =
+                        ColorTextureVerts.Select(v => Vector3.Distance(Vector3.Zero, v.Position)).Concat(new[] {longest})
+                            .Max();
+                    break;
+                case PrimitiveFormat.NormalTexture:
+                    longest =
+                        NormalTextureVerts.Select(v => Vector3.Distance(Vector3.Zero, v.Position)).Concat(new[]
+                                                                                                              {longest})
+                            .Max();
+                    break;
+                case PrimitiveFormat.Texture:
+                    longest =
+                        TextureVerts.Select(v => Vector3.Distance(Vector3.Zero, v.Position)).Concat(new[] {longest}).Max
+                            ();
+                    break;
+            }
+
+            return longest;
+        }
+
+        #endregion
+
         #region Implemented from Basic
+
+        protected override void LoadContent()
+        {
+            effect = new BasicEffect(Game1.GameInstance.GraphicsDevice);
+            LoadPrimitive();
+        }
+
+        public override void Draw(Camera camera)
+        {
+            Game1.GameInstance.GraphicsDevice.SetVertexBuffer(VertexBuffer);
+
+            effect.World = World;
+            effect.View = camera.View;
+            effect.Projection = camera.Projection;
+            foreach (EffectPass pass in effect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+
+                switch (Format)
+                {
+                    case PrimitiveFormat.Color:
+                        Game1.GameInstance.GraphicsDevice.DrawUserPrimitives(Type, ColorVerts, VertexOffset,
+                                                                             PrimitiveCount);
+                        break;
+                    case PrimitiveFormat.ColorTexture:
+                        Game1.GameInstance.GraphicsDevice.DrawUserPrimitives(Type, ColorTextureVerts, VertexOffset,
+                                                                             PrimitiveCount);
+                        break;
+                    case PrimitiveFormat.NormalTexture:
+                        Game1.GameInstance.GraphicsDevice.DrawUserPrimitives(Type, NormalTextureVerts, VertexOffset,
+                                                                             PrimitiveCount);
+                        break;
+                    case PrimitiveFormat.Texture:
+                        Game1.GameInstance.GraphicsDevice.DrawUserPrimitives(Type, TextureVerts, VertexOffset,
+                                                                             PrimitiveCount);
+                        break;
+                }
+            }
+        }
 
         protected override BoundingSphere GetBoundingSphere()
         {
-            throw new NotImplementedException();
+            return new BoundingSphere(Vector3.Zero,_boundingSphereSize );
         }
 
         #endregion
