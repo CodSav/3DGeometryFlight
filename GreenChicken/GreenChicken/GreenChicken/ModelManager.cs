233e2c6f9ea47d06dce887a76fbd215d5fbbf8db
ZAD-Man <The.ZAD.One@GMail.com>
2012-06-08 09:55:04 -0600

    Added Audio, an empty ModelManager (had some stuff in it but ended up getting rid of it). Mostly a commit to make it let me pull. :P

---
+++ GreenChicken/GreenChicken/GreenChicken/ModelManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GreenChicken
+{
+    internal class ModelManager : DrawableGameComponent
+    {
+        public ModelManager(Game game) : base(game)
+        {
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ GreenChicken/GreenChicken/GreenChicken/PlayerModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GreenChicken
+{
+    class PlayerModel : BasicModel
+    {
+        private Vector3 _playerPosition = Vector3.Zero;
+        private const float PLAYER_SPEED = 1.5f;
+
+        public PlayerModel(Model m, Game game, bool isCollidable = true) : base(m, game, isCollidable)
+        {
+        }
+        
+        public override void Update()
+        {
+            //TODO: Change to move with camera once we have it
+            InputManager inputManager = ((Game1) Game).InputManager;
+
+            if (inputManager.KeyDown(InputManager.GameKeyCodes.MOVE_LEFT))
+            {
+                _playerPosition.X -= PLAYER_SPEED;
+            }
+            else if (inputManager.KeyDown(InputManager.GameKeyCodes.MOVE_RIGHT))
+            {
+                _playerPosition.X += PLAYER_SPEED;
+            }
+
+            if (inputManager.KeyDown(InputManager.GameKeyCodes.MOVE_BACKWARD))
+            {
+                _playerPosition.Z -= PLAYER_SPEED;
+            }
+            else if (inputManager.KeyDown(InputManager.GameKeyCodes.MOVE_FORWARD))
+            {
+                _playerPosition.Z += PLAYER_SPEED;
+            }
+
+            if (inputManager.KeyDown(InputManager.GameKeyCodes.MOVE_UP))
+            {
+                _playerPosition.Y += PLAYER_SPEED;
+            }
+            else if (inputManager.KeyDown(InputManager.GameKeyCodes.MOVE_DOWN))
+            {
+                _playerPosition.Y -= PLAYER_SPEED;
+            }
+        }
+    }
+}
