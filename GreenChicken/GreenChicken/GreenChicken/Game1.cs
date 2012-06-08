062fc21753c3b3228be0f15e00a1b66b9eb3c25c
Meowfazar <bwilson@student.neumont.edu>
2012-06-08 11:24:24 -0700

    Updated game1 to include a Camera

--- GreenChicken/GreenChicken/GreenChicken/Game1.cs
+++ GreenChicken/GreenChicken/GreenChicken/Game1.cs
@@ -1,57 +1,47 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Audio;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Media;
 
 namespace GreenChicken
 {
-    public class Game1 : Microsoft.Xna.Framework.Game
+    public class Game1 : Game
     {
-        GraphicsDeviceManager graphics;
-        SpriteBatch spriteBatch;
+        public static Game1 GameInstance;
 
         public InputManager InputManager;
+        private GraphicsDeviceManager graphics;
 
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+            GameInstance = this;
         }
+
+        public Camera Camera { get; set; }
 
         protected override void Initialize()
         {
             InputManager.GetInstance(this);
             Components.Add(InputManager);
+            Camera = new Camera(this);
+            Components.Add(Camera);
 
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
-            // Create a new SpriteBatch, which can be used to draw textures.
-            spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: use this.Content to load your game content here
+            
         }
-protected override void UnloadContent()
+
+        protected override void UnloadContent()
         {
-            // TODO: Unload any non ContentManager content here
         }
 
         protected override void Update(GameTime gameTime)
         {
-            // Allows the game to exit
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
-                this.Exit();
-
-            // TODO: Add your update logic here
-
             base.Update(gameTime);
         }
 
@@ -63,9 +53,12 @@ protected override void UnloadContent()
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
-
             base.Draw(gameTime);
+        }
+
+        public void GetZoneOfPosition(Vector3 Position)
+        {
+            throw new NotImplementedException();
         }
     }
 }
