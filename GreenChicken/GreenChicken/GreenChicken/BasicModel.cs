4a0418e263ec0d7fa00f4b8f4dc94bdf1cceee66
Meowfazar <bwilson@student.neumont.edu>
2012-06-08 11:31:39 -0700

    Update of basic model

--- GreenChicken/GreenChicken/GreenChicken/BasicModel.cs
+++ GreenChicken/GreenChicken/GreenChicken/BasicModel.cs
@@ -1,45 +1,163 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
-namespace GreenChicken
+namespace _3D_Madness
 {
-    public abstract class BasicModel : Basic
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
     {
-        public Model Model { get { return GetModel(); } protected set { _model = value; } }
-        protected abstract Model GetModel();
-        protected Model _model;
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
 
-        public bool Render = true;
+        // Game camera
+        Camera camera;
 
-        #region Implemented from Basic
+        // Vertex data
+        VertexPositionTexture[] verts;
+        VertexBuffer vertexBuffer;
 
-        protected override BoundingSphere GetBoundingSphere()
+        // Effect
+        BasicEffect effect;
+
+        // Movement and rotation stuff
+        Matrix worldTranslation = Matrix.Identity;
+        Matrix worldRotation = Matrix.Identity;
+
+        // Texture info
+        Texture2D texture;
+
+        public Game1()
         {
-            var boneTransforms = new Matrix[Model.Bones.Count];
-            Model.CopyAbsoluteBoneTransformsTo(boneTransforms);
-            Vector3 modelCenter = (from mesh in Model.Meshes
-                                   let meshBounds = mesh.BoundingSphere
-                                   let transform = boneTransforms[mesh.ParentBone.Index]
-                                   select Vector3.Transform(meshBounds.Center, transform)).Aggregate(Vector3.Zero,
-                                                                                                     (current,
-                                                                                                      meshCenter) =>
-                                                                                                     current +
-                                                                                                     meshCenter);
-
-            modelCenter /= Model.Meshes.Count;
-
-            float modelRadius = (from mesh in Model.Meshes
-                                 let meshBounds = mesh.BoundingSphere
-                                 let transform = boneTransforms[mesh.ParentBone.Index]
-                                 let meshCenter = Vector3.Transform(meshBounds.Center, transform)
-                                 let transformScale = transform.Forward.Length()
-                                 select (meshCenter - modelCenter).Length() + (meshBounds.Radius * transformScale)).Concat
-                (new[] { 0f }).Max();
-
-            return new BoundingSphere { Center = modelCenter, Radius = modelRadius };
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
         }
 
-        #endregion
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // Initialize camera
+            camera = new Camera(this, new Vector3(0, 0, 5),
+                Vector3.Zero, Vector3.Up);
+            Components.Add(camera);
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // Initialize vertices
+            verts = new VertexPositionTexture[4];
+            verts[0] = new VertexPositionTexture(
+                new Vector3(-1, 1, 0), new Vector2(0, 0));
+            verts[1] = new VertexPositionTexture(
+                new Vector3(1, 1, 0), new Vector2(1, 0));
+            verts[2] = new VertexPositionTexture(
+                new Vector3(-1, -1, 0), new Vector2(0, 1));
+            verts[3] = new VertexPositionTexture(
+                new Vector3(1, -1, 0), new Vector2(1, 1));
+
+            // Set vertex data in VertexBuffer
+            vertexBuffer = new VertexBuffer(GraphicsDevice, typeof(VertexPositionTexture), verts.Length, BufferUsage.None);
+            vertexBuffer.SetData(verts);
+
+            // Initialize the BasicEffect
+            effect = new BasicEffect(GraphicsDevice);
+
+            // Load texture
+            texture = Content.Load<Texture2D>(@"Textures\trees");
+           
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            // Translation
+            KeyboardState keyboardState = Keyboard.GetState();
+            if (keyboardState.IsKeyDown(Keys.Left))
+                worldTranslation *= Matrix.CreateTranslation(-.01f, 0, 0);
+            if (keyboardState.IsKeyDown(Keys.Right))
+                worldTranslation *= Matrix.CreateTranslation(.01f, 0, 0);
+
+            // Rotation
+            worldRotation *= Matrix.CreateFromYawPitchRoll(
+                MathHelper.PiOver4 / 60,
+                0, 
+                0);
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            
+
+            // Set the vertex buffer on the GraphicsDevice
+            GraphicsDevice.SetVertexBuffer(vertexBuffer);
+
+            //Set object and camera info
+            effect.World = worldRotation * worldTranslation * worldRotation;
+            effect.View = camera.view;
+            effect.Projection = camera.projection;
+            effect.Texture = texture;
+            effect.TextureEnabled = true;
+
+            // Begin effect and draw for each pass
+            foreach (EffectPass pass in effect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+
+                GraphicsDevice.DrawUserPrimitives<VertexPositionTexture>
+                    (PrimitiveType.TriangleStrip, verts, 0, 2);
+
+            }
+
+
+            base.Draw(gameTime);
+        }
     }
 }
