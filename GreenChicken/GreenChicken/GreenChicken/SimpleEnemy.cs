ce8ec46e8e3ae4d859e298417612cbedc7e0b640
Meowfazar <bwilson@student.neumont.edu>
2012-06-15 08:00:14 -0700

    SimpleEnemy now has movement

--- GreenChicken/GreenChicken/GreenChicken/SimpleEnemy.cs
+++ GreenChicken/GreenChicken/GreenChicken/SimpleEnemy.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace GreenChicken
@@ -9,6 +10,27 @@ namespace GreenChicken
         {
             IsCollidable = isCollidable;
             LoadPrimitive();
+            switch (new Random().Next(0, 6))
+            {
+                case 0:
+                    moveType = MoveType.Static;
+                    break;
+                case 1:
+                    moveType = MoveType.Simple2;
+                    break;
+                case 2:
+                    moveType = MoveType.Simple;
+                    break;
+                case 3:
+                    moveType = MoveType.Random;
+                    break;
+                case 4:
+                    moveType = MoveType.Simple3;
+                    break;
+                case 5:
+                    moveType = MoveType.Follow;
+                    break;
+            }
         }
 
         #region Overrides of BasicPrimitive
@@ -17,11 +39,11 @@ namespace GreenChicken
         {
             Type = PrimitiveType.TriangleStrip;
             ColorVerts = new VertexPositionColor[5];
-            ColorVerts[0] = new VertexPositionColor(new Vector3(2, 2, 0), Color.BlueViolet);
-            ColorVerts[1] = new VertexPositionColor(new Vector3(2, 2, -2), Color.BlueViolet);
-            ColorVerts[2] = new VertexPositionColor(new Vector3(0, 0, 1), Color.Green);
-            ColorVerts[3] = new VertexPositionColor(new Vector3(-2, 2, 0), Color.BlueViolet);
-            ColorVerts[4] = new VertexPositionColor(new Vector3(-2, 2, -2), Color.BlueViolet);
+            ColorVerts[0] = new VertexPositionColor(new Vector3(4, 4, 0), Color.BlueViolet);
+            ColorVerts[1] = new VertexPositionColor(new Vector3(4, 4, -4), Color.BlueViolet);
+            ColorVerts[2] = new VertexPositionColor(new Vector3(7, 0, 7), Color.Green);
+            ColorVerts[3] = new VertexPositionColor(new Vector3(-4, 4, 0), Color.BlueViolet);
+            ColorVerts[4] = new VertexPositionColor(new Vector3(-4, 4, -4), Color.BlueViolet);
         }
 
         #endregion
